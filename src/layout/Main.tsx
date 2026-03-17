826ad6fb504ec553c72bfad90e1407f126c79081
Guilherme <guilherme_braga100@hotmail.com>
2026-03-17 21:39:55 +0000

    feat: Implementação de navbar e sidebar.

--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,5 +1,38 @@
+import { Box } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { Sidebar, Navbar } from "./elements";
+
 export default function Layout() {
   return (
-    <></>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+    >
+      <Navbar />
+      <Box sx={{ display: "flex", flex: 1, minHeight: 0 }}>
+        <Sidebar />
+        <Box
+          component="main"
+          sx={(t) => ({
+            bgcolor: t.palette.action.hover,
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            pt: 10,
+            paddingBottom: 2,
+            paddingX: 2,
+            overflow: "auto",
+          })}
+        >
+          <Outlet />
+        </Box>
+      </Box>
+    </Box>
   );
 }
